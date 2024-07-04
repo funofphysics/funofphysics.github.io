555af20ae1ffc7f59a4473067a081cacc8264a48
Fun Of Physics <funofphysics@rediffmail.com>
2024-07-04 21:59:21 +0530

    fix js

--- js/shareUrl.js
+++ js/shareUrl.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     document.getElementById('share-facebook').href = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(url);
     document.getElementById('share-twitter').href = 'https://twitter.com/intent/tweet?url=' + encodeURIComponent(url) + '&text=' + encodeURIComponent(title);
-    document.getElementById('share-reddit').href = 'https://www.reddit.com/submit?url='+encodeURIComponent(url) + '&title' +  encodeURIComponent(title)
+    document.getElementById('share-reddit').href = 'https://www.reddit.com/submit?url='+encodeURIComponent(url) + '&title' +  encodeURIComponent(title);
     document.getElementById('share-linkedin').href = 'https://www.linkedin.com/shareArticle?mini=true&url=' + encodeURIComponent(url) + '&title=' + encodeURIComponent(title);
     document.getElementById('share-email').href = 'mailto:?subject=' + encodeURIComponent(title) + '&body=' + encodeURIComponent(url);
     document.getElementById('share-whatsapp').href = 'https://api.whatsapp.com/send?text=' + encodeURIComponent(title + ' ' + url);
